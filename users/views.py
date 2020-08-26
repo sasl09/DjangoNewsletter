597d8b1f9a779e89ac33d094c4aa572a93fadf21
Armandus <sasl09@outlook.com>
2020-08-26 10:36:47 -0500

    Removed API login auth configuration for all views

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.models import User, Group
+from users.models import Profile
 
 
 def register(request):
@@ -29,7 +30,49 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
-@login_required
+# @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+
+def profiles(request):
+    context = {
+        'profiles': Profile.objects.all(),
+    }
+    template = 'users/profiles.html'
+    return render(request, template, context)
+
+
+# def author_profile(request):
+#     req = str(request)
+#     req = req[30:-3].capitalize()
+#     context = {
+#         'authors': Profile.objects.filter(user__author=req),
+#     }
+#     template = 'users/profile.html'
+#     return render(request, template, context)
+
+
+def update_profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/update_profile.html', context)
 
